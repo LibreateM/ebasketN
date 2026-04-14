3d96ca6474af5b9e145d6c67d061f73d0fa35856
LibreateM <moksh8394@gmail.com>
2026-04-14 17:49:02 +0530

    Update settings.py

--- ebasket/settings.py
+++ ebasket/settings.py
@@ -132,6 +132,11 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
