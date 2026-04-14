47bef8d6aa6bb6ad200f3949b62f832da7cf6d74
LibreateM <moksh8394@gmail.com>
2026-04-14 17:25:41 +0530

    Update settings.py

--- ebasket/settings.py
+++ ebasket/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
