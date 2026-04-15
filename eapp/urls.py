ec33cf1dddb3713c00cf26e8e1d20c2624750e47
LibreateM <moksh8394@gmail.com>
2026-04-15 17:24:31 +0530

    Update urls.py

--- eapp/urls.py
+++ eapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
+  path('test-storage/', test_storage),
   path('test-cloud/', test_cloudinary),
   path('', index, name="index"),
   path('about/', about, name="about"),
