069d5472f147cd686fda14016796e86ca3ddd9d2
LibreateM <moksh8394@gmail.com>
2026-04-15 17:21:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+from django.core.files.storage import default_storage
+print(default_storage)
